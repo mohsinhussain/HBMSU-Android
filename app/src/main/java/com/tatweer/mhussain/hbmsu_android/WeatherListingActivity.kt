b9d0a7661733a9994eecec4705b6d97fb0f4b9f4
Mohsin Hussain <m.hussain@tatweer-co.ae>
2018-08-20 15:56:39 +0400

    Initial Commit

---
+++ app/src/main/java/com/tatweer/mhussain/hbmsu_android/WeatherListingActivity.kt
@@ -0,0 +1,12 @@
+package com.tatweer.mhussain.hbmsu_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WeatherListingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather_listing)
+    }
+}
